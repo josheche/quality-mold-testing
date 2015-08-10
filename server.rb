6282559218b55074e17c7ac4bbb91f5bdc0800a0
Joshua Echeverri <josheche@gmail.com>
2015-08-10 16:02:54 -0400

    connected pony mailer with account

--- server.rb
+++ server.rb
@@ -10,4 +10,15 @@ get '/services' do
 end
 
 post '/send' do
+  content_type :json
+
+  # Define your message parameters
+  begin
+    Pony.mail(:to => 'descarda@qualitymoldtesting.com', :html_body => "<ul><li>Name: #{params['name']},</li> <li>Email: #{params['email']},</li> <li>Message: #{params['message']}</li>", :body => "In case you can't read HTML: Name: #{params['name']}, Email: #{params['email']}, Message: #{params['message']}")
+
+    { :status => '200' }.to_json
+  rescue Exception => e
+    { :status => '500' }.to_json
+  end
+
 end
