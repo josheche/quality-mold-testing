29da066474828aab56a639e15362fd78d8cb020d
Dan Vera <dan@yovu.co>
2015-08-18 17:18:39 -0400

    deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,18 @@
+SSHKit.config.command_map[:rake] = "bundle exec rake"
+
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'mold'
+set :repo_url, 'git@github.com:entropy-software/quality-mold-testing.git'
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, 'master'
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/home/apps/mold'
 
-# Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
+
+set :stages, ["production"]
 
 # Default value for :format is :pretty
 # set :format, :pretty
@@ -19,20 +20,38 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # Default value for :log_level is :debug
 # set :log_level, :debug
 
-# Default value for :pty is false
-# set :pty, true
+set :unicorn_config_path, "config/unicorn.rb"
+
+set :pty, true
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, %w{config/deploy/nginx.conf.erb}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, %w{log tmp/pids public}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_ruby, '2.2.2'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
+
+# nginx
+set :nginx_service_path, "/etc/init.d/nginx"
+set :nginx_roles, :web
+set :nginx_root_path, "/etc/nginx"
+set :nginx_sites_available, "sites-available"
+set :nginx_sites_enabled, "sites-enabled"
+set :nginx_template, "#{stage_config_path}/nginx.conf.erb"
+set :app_server, true
+set :app_server_socket, "/tmp/unicorn-#{fetch :application}.socket"
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 namespace :deploy do
 
